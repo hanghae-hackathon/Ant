2747b5696ec7acf858d402b0ded1b4eaf2f75adb
Younglong <kyl8555@gmail.com>
2024-06-01 00:48:39 +0900

    init: project init (2)

---
+++ src/main/java/com/hanghae/hackathon/Ant/AntApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.hackathon.Ant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntApplication.class, args);
+	}
+
+}
