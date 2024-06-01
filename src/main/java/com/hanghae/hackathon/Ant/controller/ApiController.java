b18646ebc2ca7a955b8c85cc9207e611745b0184
TaeHan Lee <lth9312@gmail.com>
2024-06-01 14:37:52 +0900

    test: 이미지 테스트 5

--- src/main/java/com/hanghae/hackathon/Ant/controller/ApiController.java
+++ src/main/java/com/hanghae/hackathon/Ant/controller/ApiController.java
@@ -1,12 +1,17 @@
 package com.hanghae.hackathon.Ant.controller;
 
 import com.hanghae.hackathon.Ant.service.RecycleService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ApiController {
@@ -14,16 +19,31 @@ public class ApiController {
     @Autowired
     RecycleService service;
 
-    @GetMapping("/ask")
-    public void ask() {
-        service.getResult();
+//    @GetMapping("/ask")
+//    public void ask() {
+//        service.getResult();
+//    }
+//
+//    @PostMapping("/question")
+//    public String question(
+//            @RequestPart(required = false) String reqInfo,
+//            @RequestPart(required = false) MultipartFile attachFile) {
+//
+//        return service.getAnswer(reqInfo, attachFile);
+//    }
+
+    @PostMapping("/ask")
+    public ResponseEntity ask(MultipartHttpServletRequest request){
+        Map<String,Object> resultMap = new HashMap<String,Object>();
+        resultMap.put("result", service.getAnswer(null, request.getFile("imageFile")));
+        return new ResponseEntity<>(resultMap, HttpStatus.OK);
     }
 
-    @PostMapping("/question")
-    public String question(
-            @RequestPart(required = false) String reqInfo,
-            @RequestPart(required = false) MultipartFile attachFile) {
-
-        return service.getAnswer(reqInfo, attachFile);
+    @PostMapping("/askText")
+    public ResponseEntity askText(HttpServletRequest request, @RequestBody Map<String, Object> param){
+        Map<String,Object> resultMap = new HashMap<String,Object>();
+        String at = param.getOrDefault("askText", "").toString();
+        resultMap.put("result", service.getAnswer(param.getOrDefault("askText", "").toString(), null));
+        return new ResponseEntity<>(resultMap, HttpStatus.OK);
     }
 }
